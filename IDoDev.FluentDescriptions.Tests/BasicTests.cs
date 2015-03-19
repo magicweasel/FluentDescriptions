c4fa845ac06b625014d402b6ca8b1654ce77f160
Andy Clarke <idodevdotnet@users.noreply.github.com>
2015-03-19 21:56:37 +0000

    Add initial code

---
+++ IDoDev.FluentDescriptions.Tests/BasicTests.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using FluentDescriptions;
+using NUnit.Framework;
+
+namespace fluent_descriptions_tests
+{
+	[TestFixture]	
+    public class when_describing_calculations
+    {
+		[Test]
+		public void then_integers_sum_correctly()
+		{
+			123.Should().Be(equal_to.a("value of").of(100) + a._("another value").of(23));
+		}
+
+		[Test]
+		public void then_doubles_sum_correctly()
+		{
+			333.33d.Should().Be((equal_to.a("value of").of(110.01d) + a._("another value").of(1.1d)) * the._("multiplier").of(3d));
+		}
+
+		[Test]
+		public void then_integers_and_doubles_sum_correctly()
+		{
+			500.99d.Should().Be(equal_to.a("value of").of(500) + a._("another value").of(0.99d));
+		}
+
+		[Test]
+		public void then_floats_sum_correctly()
+		{
+			444.44f.Should().Be(Equal_to.the("value of").of(444.40f) + an._("extra value of").of(0.04f));
+		}
+
+		[Test]
+		public void then_decimals_sum_correctly()
+		{
+			33.55m.Should().Be(Equal_to.the("value of").of(33.5m) + and_a._("value of").of(0.05m));
+		}
+
+		[Test]
+		public void then_decimals_and_integers_sum_correctly()
+		{
+			12.34m.Should().Be(Equal_to.the("value of").of(12) + and_an._("extra value of").of(0.34m));
+		}
+    }
+}
---
+++ IDoDev.FluentDescriptions/FluentDescriptions.cs
@@ -0,0 +1,60 @@
+﻿namespace FluentDescriptions
+{
+	public class equal_to
+	{
+		public static ofValue the(string what)
+		{
+			return new ofValue();
+		}
+
+		public static ofValue a(string what)
+		{
+			return new ofValue();
+		}
+	}
+
+	public class Equal_to
+	{
+		public static ofValue the(string what)
+		{
+			return new ofValue();
+		}
+
+		public static ofValue a(string what)
+		{
+			return new ofValue();
+		}
+	}
+
+	public class ofValue
+	{
+		public T of<T>(T value)
+		{
+			return value;
+		}
+	}
+
+	public class a
+	{
+		public static ofValue _(string what)
+		{
+			return new ofValue();
+		}
+	}
+
+	public class an : a
+	{
+	}
+
+	public class the : a
+	{
+	}
+
+	public class and_a : a
+	{
+	}
+
+	public class and_an : a
+	{
+	}
+}
